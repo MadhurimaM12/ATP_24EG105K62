091bf334672976472f3d6f6fd527a8e42afc9c12
MadhurimaM12 <mandamadhurima2006@gmail.com>
2026-03-24 22:37:20 +0530

    Create Navbar.jsx

---
+++ Week5/react2/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1 className="logo">LOGO</h1>
+       <div className="nav-links">
+        <button>Home</button>
+        <button>Signup</button>
+        <button>Login</button>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
