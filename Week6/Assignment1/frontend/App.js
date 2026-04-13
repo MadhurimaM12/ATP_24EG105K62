e6e6ce7772783a3f367c89d384a69151bb6eefb3
MadhurimaM12 <mandamadhurima2006@gmail.com>
2026-04-13 23:06:53 +0530

    Create App.js

---
+++ Week6/Assignment1/frontend/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import UserManagement from './components/UserManagement';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>User Management System</h1>
+      <UserManagement />
+    </div>
+  );
+}
+
+export default App;
